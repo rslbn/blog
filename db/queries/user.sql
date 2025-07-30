6b7f81b9574d3049f111b11d543c78dd65718d6a
rslbn <ronyslbn7@gmail.com>
2025-07-30 22:32:54 +0700

    update: add new queries

--- db/queries/user.sql
+++ db/queries/user.sql
@@ -4,6 +4,10 @@ SELECT * FROM users WHERE username = $1 LIMIT 1;
 SELECT * FROM users WHERE email = $1 LIMIT 1;
 -- name: ListUsers :many
 SELECT * FROM users;
+-- name: UserExistByUsername :one
+SELECT COUNT(*) > 0 AS is_exist FROM users WHERE username = $1;
+-- name: UserExistsByEmail :one
+SELECT COUNT(*) > 0 AS is_exist FROM users WHERE email = $1;
 -- name: InsertUser :one
 INSERT INTO users (username, email, password)
 VALUES ($1, $2, $3) RETURNING *;
