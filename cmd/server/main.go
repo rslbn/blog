fca6e5a411bdd6934f570830abf65a25721fdd0f
rslbn <ronyslbn7@gmail.com>
2025-07-30 22:32:29 +0700

    setup: db connection, base server, router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/rslbn/blog/internal/app"
+	"github.com/rslbn/blog/pkg/database"
 )
 
 func main() {
-	router := app.NewRouter()
+	ctx := context.Background()
+	dbConn, err := database.NewPostgresDB(ctx, os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatalf("failed to load the database: %v", err)
+	}
+	defer dbConn.Close()
+	fmt.Println("Database pool connection is established.")
+	router := app.NewRouter(dbConn)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- internal/app/router.go
+++ internal/app/router.go
@@ -3,20 +3,27 @@ package app
 import (
 	"net/http"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rslbn/blog/internal/app/handler"
 	"github.com/rslbn/blog/internal/app/service"
+	"github.com/rslbn/blog/internal/app/web"
+	db "github.com/rslbn/blog/postgres"
 )
 
-func NewRouter() http.Handler {
+func NewRouter(dbConn *pgxpool.Pool) http.Handler {
 	mux := http.NewServeMux()
 
-	userService := service.NewUserService()
+	sqlcQueries := db.New(dbConn)
+
+	userService := service.NewUserService(sqlcQueries)
 	userHandler := handler.NewUserHandler(userService)
 
-	authService := service.NewAuthService(userService)
-	authHandler := handler.NewAuthHandler(authService)
+	// authService := service.NewAuthService(userService)
+	// authHandler := handler.NewAuthHandler(authService)
 
-	mux.HandleFunc("GET /users/{username}", userHandler.GetUserByUsername)
-	mux.HandleFunc("POST /auth/login", authHandler.Login)
+	mux.HandleFunc("GET /users", web.HandlerAdapter(userHandler.GetAll))
+	mux.HandleFunc("GET /users/{username}", web.HandlerAdapter(userHandler.GetUserByUsername))
+	// mux.HandleFunc("POST /auth/login", authHandler.Login)
+	mux.HandleFunc("POST /auth/register", web.HandlerAdapter(userHandler.Register))
 	return mux
 }
